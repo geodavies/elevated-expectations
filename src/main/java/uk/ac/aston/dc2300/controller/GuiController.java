0ef8c09840e3451849fa87366f51b1e3a4fb7eaf
Daniel Cotton <dan@daniel-cotton.co.uk>
2017-04-07 21:15:37 +0100

    initial frame

--- src/main/java/uk/ac/aston/dc2300/controller/GuiController.java
+++ src/main/java/uk/ac/aston/dc2300/controller/GuiController.java
@@ -3,6 +3,7 @@ package uk.ac.aston.dc2300.controller;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import uk.ac.aston.dc2300.component.Simulation;
+import uk.ac.aston.dc2300.gui.ConfigFrame;
 
 /**
  * <p>
@@ -17,15 +18,20 @@ public class GuiController implements ApplicationController {
     private static final Logger LOGGER = LogManager.getLogger(GuiController.class);
 
     private Simulation simulation;
+    private boolean configured;
 
     public GuiController() {
         LOGGER.info("Initializing application in 'gui' mode");
+        configured = false;
         simulation = new Simulation();
     }
 
     @Override
     public void start() {
         // TODO: Implement
+        ConfigFrame f = new ConfigFrame();
+
+        f.show();
     }
 
 }
