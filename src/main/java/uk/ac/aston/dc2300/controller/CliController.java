0cf4aac814332a53d89c55e577e9952d70bf4408
Daniel Cotton <dan@daniel-cotton.co.uk>
2017-04-08 20:55:23 +0100

    refactored clicontroller code to reduce duplication, created CLIUtils

--- src/main/java/uk/ac/aston/dc2300/controller/CliController.java
+++ src/main/java/uk/ac/aston/dc2300/controller/CliController.java
@@ -4,6 +4,7 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import uk.ac.aston.dc2300.component.Simulation;
 import uk.ac.aston.dc2300.model.configuration.SimulationConfiguration;
+import uk.ac.aston.dc2300.utility.CliUtils;
 
 import java.math.BigDecimal;
 import java.util.Scanner;
@@ -43,6 +44,8 @@ public class CliController implements ApplicationController {
 
         Scanner scanner = new Scanner(System.in);
 
+        CliUtils cliMediator = new CliUtils();
+
         // Initialize variables
         BigDecimal floorChangeProbability = null;
         BigDecimal clientArrivalProbability = null;
@@ -54,169 +57,28 @@ public class CliController implements ApplicationController {
         int elevatorCapacity = -1;
 
         // Get floor change probability
-        while (floorChangeProbability == null) {
-            System.out.printf("Floor change probability (p) [0.01]: ");
-            // Take user input
-            String floorChangeProbabilityInput = scanner.nextLine();
-            // If left empty, set to default value
-            if (floorChangeProbabilityInput.isEmpty()) {
-                floorChangeProbability = new BigDecimal("0.01");
-            } else {
-                try {
-                    // Check value is valid decimal
-                    BigDecimal validationDecimal = new BigDecimal(floorChangeProbabilityInput);
-                    // Check value is between 0 and 1
-                    if (validationDecimal.compareTo(BigDecimal.ZERO) < 0 || validationDecimal.compareTo(BigDecimal.ONE) > 0) {
-                        System.out.println("Probability must be a decimal between 0 and 1");
-                        continue;
-                    }
-                    floorChangeProbability = validationDecimal;
-                } catch (NumberFormatException e) {
-                    System.out.println("Probability must be a decimal between 0 and 1");
-                }
-            }
-        }
+        floorChangeProbability = cliMediator.readBigDecimalViaCli("Floor Change probability (p)", new BigDecimal("0.01"));
 
         // Get client arrival probability
-        while (clientArrivalProbability == null) {
-            System.out.printf("Client arrival probability (q) [0.005]: ");
-            // Get user input
-            String clientArrivalProbablityInput = scanner.nextLine();
-            // If left empty, set to default value
-            if (clientArrivalProbablityInput.isEmpty()) {
-                clientArrivalProbability = new BigDecimal("0.005");
-            } else {
-                try {
-                    // Check value is valid decimal
-                    BigDecimal validationDecimal = new BigDecimal(clientArrivalProbablityInput);
-                    // Check value is between 0 and 1
-                    if (validationDecimal.compareTo(BigDecimal.ZERO) < 0 || validationDecimal.compareTo(BigDecimal.ONE) > 0) {
-                        System.out.println("Probability must be a decimal between 0 and 1");
-                        continue;
-                    }
-                    clientArrivalProbability = validationDecimal;
-                } catch (NumberFormatException e) {
-                    System.out.println("Probability must be a decimal between 0 and 1");
-                }
-            }
-        }
+        clientArrivalProbability = cliMediator.readBigDecimalViaCli("Client arrival probability (q) ",  new BigDecimal("0.005"));
 
         // Get randomization seed
-        while (seed == -1) {
-            System.out.printf("Randomization seed [420]: ");
-            // Get user input
-            String seedInput = scanner.nextLine();
-            // If left empty, set to default value
-            if (seedInput.equals("")) {
-                seed = 420;
-            } else {
-                try {
-                    // Check if valid long
-                    long validationLong = Long.parseLong(seedInput);
-                    seed = validationLong;
-                } catch (NumberFormatException e) {
-                    System.out.println("Must be a valid whole number");
-                }
-            }
-        }
+        seed = cliMediator.readLongViaCli("Randomization seed", 420);
 
         // Get number of employees
-        while (numEmployees == -1) {
-            System.out.printf("Number of employees [10]: ");
-            // Get user input
-            String numEmployeesInput = scanner.nextLine();
-            // If left empty, set to default value
-            if (numEmployeesInput.equals("")) {
-                numEmployees = 10;
-            } else {
-                try {
-                    // Check if valid int
-                    int validationInt = Integer.parseInt(numEmployeesInput);
-                    // Check greater than 0
-                    if(validationInt < 0){
-                        System.out.println("Must be greater than or equal to 0");
-                        continue;
-                    }
-                    numEmployees = validationInt;
-                } catch (NumberFormatException e) {
-                    System.out.println("Must be a valid whole number");
-                }
-            }
-        }
+        numEmployees = cliMediator.readIntegerViaCli("Number of employees in building", 10);
 
         // Get number of developers
-        while (numDevelopers == -1) {
-            System.out.printf("Number of developers [10]: ");
-            // Get user input
-            String numDevelopersInput = scanner.nextLine();
-            // If left empty, set to default value
-            if (numDevelopersInput.equals("")) {
-                numDevelopers = 10;
-            } else {
-                try {
-                    // Check if valid int
-                    int validationInt = Integer.parseInt(numDevelopersInput);
-                    // Check greater than 0
-                    if(validationInt < 0){
-                        System.out.println("Must be greater than or equal to 0");
-                        continue;
-                    }
-                    numDevelopers = validationInt;
-                } catch (NumberFormatException e) {
-                    System.out.println("Must be a valid whole number");
-                }
-            }
-        }
+        numDevelopers = cliMediator.readIntegerViaCli("Number of developers in building", 10);
 
         // Get number of floors
-        while (numFloors == -1) {
-            System.out.printf("Number of floors in building [6]: ");
-            // Get user input
-            String numFloorsInput = scanner.nextLine();
-            // If left empty, set to default value
-            if (numFloorsInput.equals("")) {
-                numFloors = 6;
-            } else {
-                try {
-                    // Check if valid int
-                    int validationInt = Integer.parseInt(numFloorsInput);
-                    // Check greater than 0
-                    if(validationInt < 0){
-                        System.out.println("Must be greater than or equal to 0");
-                        continue;
-                    }
-                    numFloors = validationInt;
-                } catch (NumberFormatException e) {
-                    System.out.println("Must be a valid whole number");
-                }
-            }
-        }
+        numFloors = cliMediator.readIntegerViaCli("Number of floors in building", 6);
 
         // Get elevator capacities
-        while (elevatorCapacity == -1) {
-            System.out.printf("Max Capacity of elevators in building [4]: ");
-            // Get user input
-            String elevatorCapacityInput = scanner.nextLine();
-            // If left empty, set to default value
-            if (elevatorCapacityInput.equals("")) {
-                elevatorCapacity = 4;
-            } else {
-                try {
-                    // Check if valid int
-                    int validationInt = Integer.parseInt(elevatorCapacityInput);
-                    // Check greater than 0
-                    if(validationInt < 0){
-                        System.out.println("Must be greater than or equal to 0");
-                        continue;
-                    }
-                    elevatorCapacity = validationInt;
-                } catch (NumberFormatException e) {
-                    System.out.println("Must be a valid whole number");
-                }
-            }
-        }
+        elevatorCapacity = cliMediator.readIntegerViaCli("Max Capacity of Elevators", 4);
 
         // Create some space to improve legibility
+        System.out.print("Your chosen capacity: " + elevatorCapacity);
         System.out.print("\n");
 
         return new SimulationConfiguration(floorChangeProbability, clientArrivalProbability, seed, numEmployees, numDevelopers, numFloors, elevatorCapacity);
---
+++ src/main/java/uk/ac/aston/dc2300/utility/CliUtils.java
@@ -0,0 +1,120 @@
+package uk.ac.aston.dc2300.utility;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+/**
+ * A utility class to perform repetitive CLI operations.
+ *
+ * @author Dan Cotton
+ * @since 08/04/17.
+ */
+public class CliUtils {
+
+    private Scanner cliScanner;
+
+    public CliUtils () {
+        cliScanner = new Scanner(System.in);
+    }
+
+
+    /**
+     * Uses CLI to gain integer input from user. Will prompt, verify
+     * and return result or defaultValue where appropriate.
+     *
+     * @param prompt a brief description of the field (used as prompt)
+     * @param defaultValue the default integer to be used if input not provided
+     * @return the integer result as decided by the user
+     */
+    public int readIntegerViaCli(String prompt, int defaultValue){
+        int result = -1;
+        while (result == -1) {
+            System.out.printf(prompt + " [" + defaultValue + "]: ");
+            // Get user input
+            String resultInput = cliScanner.nextLine();
+            // If left empty, set to default value
+            if (resultInput.equals("")) {
+                result = defaultValue;
+            } else {
+                try {
+                    // Check if valid int
+                    int validationInt = Integer.parseInt(resultInput);
+                    // Check greater than 0
+                    if(validationInt < 0){
+                        System.out.println("Must be greater than or equal to 0");
+                        continue;
+                    }
+                    result = validationInt;
+                } catch (NumberFormatException e) {
+                    System.out.println("Must be a valid whole number");
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Uses CLI to gain long input from user. Will prompt, verify
+     * and return result or defaultValue where appropriate.
+     *
+     * @param prompt a brief description of the field (used as prompt)
+     * @param defaultValue the default long to be used if input not provided
+     * @return the long result as decided by the user
+     */
+    public long readLongViaCli(String prompt, long defaultValue){
+        long result = -1;
+        while (result == -1) {
+            System.out.printf(prompt + " [" + defaultValue + "]: ");
+            // Get user input
+            String resultInput = cliScanner.nextLine();
+            // If left empty, set to default value
+            if (resultInput.equals("")) {
+                result = defaultValue;
+            } else {
+                try {
+                    // Check if valid long
+                    long validationLong = Long.parseLong(resultInput);
+                    result = validationLong;
+                } catch (NumberFormatException e) {
+                    System.out.println("Must be a valid whole number");
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Uses CLI to gain BigDecimal input from user. Will prompt, verify
+     * and return result or defaultValue where appropriate.
+     *
+     * @param prompt a brief description of the field (used as prompt)
+     * @param defaultValue the default BigDecimal to be used if input not provided
+     * @return the BigDecimal result as decided by the user
+     */
+    public BigDecimal readBigDecimalViaCli(String prompt, BigDecimal defaultValue){
+        BigDecimal result = null;
+        while (result == null) {
+            System.out.printf(prompt + " [" + defaultValue + "]: ");
+            // Get user input
+            String resultInput = cliScanner.nextLine();
+            // If left empty, set to default value
+            if (resultInput.isEmpty()) {
+                result = defaultValue;
+            } else {
+                try {
+                    // Check value is valid decimal
+                    BigDecimal validationDecimal = new BigDecimal(resultInput);
+                    // Check value is between 0 and 1
+                    if (validationDecimal.compareTo(BigDecimal.ZERO) < 0 || validationDecimal.compareTo(BigDecimal.ONE) > 0) {
+                        System.out.println("Probability must be a decimal between 0 and 1");
+                        continue;
+                    }
+                    result = validationDecimal;
+                } catch (NumberFormatException e) {
+                    System.out.println("Probability must be a decimal between 0 and 1");
+                }
+            }
+        }
+        return result;
+    }
+}
