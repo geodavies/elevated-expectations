45348ff962f5ddae40ab76e02cb3cc78ab3b2d19
Daniel Cotton <dan@daniel-cotton.co.uk>
2017-04-13 21:20:02 +0100

    centered frame in desktop

--- src/main/java/uk/ac/aston/dc2300/controller/GuiController.java
+++ src/main/java/uk/ac/aston/dc2300/controller/GuiController.java
@@ -5,6 +5,7 @@ import org.apache.log4j.Logger;
 import uk.ac.aston.dc2300.gui.LandingConfig;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * <p>
@@ -23,12 +24,28 @@ public class GuiController implements ApplicationController {
     }
 
     private void startConfigUI() {
+
         JFrame configFrame = new JFrame("Elevator Simulation");
         configFrame.setContentPane(new LandingConfig().getConfigPanel());
         configFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         configFrame.setResizable(false);
         configFrame.pack();
         configFrame.setVisible(true);
+
+        /*
+            Calc and set location
+         */
+        final Dimension screenDimensions = Toolkit.getDefaultToolkit().getScreenSize();
+        final Dimension frameDimensions = configFrame.getSize();
+
+        LOGGER.debug("[GUI] Screen Dimensions: X-" + screenDimensions.width + " & Y-" + screenDimensions.height);
+        LOGGER.debug("[GUI] Frame Dimensions: X-" + frameDimensions.width + " & Y-" + frameDimensions.height);
+
+        int xPosition = (screenDimensions.width / 2) - (frameDimensions.width / 2);
+        int yPosition = (screenDimensions.height / 2) - (frameDimensions.height / 2);
+
+        LOGGER.debug("[GUI] Centering Window @ position: X-" + xPosition + " & Y-" + yPosition);
+        configFrame.setLocation(xPosition, yPosition);
     }
 
     @Override
